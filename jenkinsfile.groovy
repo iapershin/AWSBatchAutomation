ad859f26b84d91f5ec99336c2a43d94ba4d634e4
Yaroslav Pershin <62902094+mvth027@users.noreply.github.com>
2021-05-21 16:22:11 +0300

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -7,54 +7,17 @@ pipeline {
         string(name: 'IMAGE_TAG', defaultValue: 'latest', description: 'Choose Docker tag')
         booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
     }
-    tools {
-        terraform '<terraform_tool>'
-    }
-    environment {
-        TF_IN_AUTOMATION      = '1'
-    }
+
 
     stages {
         stage('Plan') {
             steps {
-                dir("environments/${ENVIRONMENT}") {
-                    sh 'terraform init -input=false'
-                    sh """terraform plan -input=false \
-                        -var 'image_tag=${IMAGE_TAG}' \
-                        --var-file=environment-variables.tfvars \
-                        -target=module.${ENVIRONMENT}-${JOB_NAME} \
-                        -out tfplan
-                    """
-                    sh 'terraform show -no-color tfplan > tfplan.txt'
+                echo 'works'
                 }
             }
         }
 
-        stage('Approval') {
-            when {
-                not {
-                    equals expected: true, actual: params.autoApprove
-                }
-            }
-            steps {
-                script {
-                    dir("environments/${ENVIRONMENT}") {
-                        def plan = readFile 'tfplan.txt'
-                        input message: "Do you want to apply the plan?",
-                                parameters: [text(name: 'Plan', description: 'Please review the plan', defaultValue: plan)]
-                    }
-                }
-            }
-        }
-
-        stage('Apply') {
-            steps {
-                dir("environments/${ENVIRONMENT}") {
-                    sh "terraform apply -input=false tfplan"
-                }
-            }
-        }
-    }
+      
 
     /*post {
         always {
